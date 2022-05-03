85921c68b1d0a3225af1c64e8deaec2e73de9482
IceToast <social@icetoast.cloud>
2022-05-03 21:42:08 +0200

    feat: ✨ Added CredentialsProvider, bcrypt and User disabled check on signIn

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from 'next-auth/providers/credentials'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
 
@@ -11,26 +12,74 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    CredentialsProvider({
+      id: 'credentials',
+      name: 'credentials',
+      credentials: {},
+      async authorize(credentials) {
+        try {
+          const user = await prisma.user.findFirst({
+            where: {
+              email: credentials.email
+            }
+          })
+
+          if (!user) return null
+
+          //Compare the hash
+          const res = await confirmPasswordHash(credentials.password, user.password)
+          if (res === true) {
+            return {
+              id: user.id,
+              name: user.name,
+              image: user.image,
+              email: user.email
+            }
+          } else {
+            console.log('Hash not matched logging in')
+
+            return null
+          }
+        } catch (err) {
+          console.log('Authorize error:', err)
+        }
+      }
     })
   ],
+  session: {
+    jwt: true,
+    maxAge: 30 * 24 * 60 * 60
+  },
   pages: {
     signIn: '/pages/login'
   },
   events: {
     createUser: async ({ user }) => {
-      console.log(user)
-
-      // add additional user fields here to db
       await prisma.user.update({
         where: {
           id: user.id
         },
+
+        // Add Custom User fields at user creation
         data: {
           createdAt: new Date(),
           updatedAt: new Date(),
           disabled: false
         }
       })
+    }
+  },
+  callbacks: {
+    signIn: async ({ user }) => {
+      try {
+        if (!user) return false
+        if (user.disabled) return false
+
+        return user
+      } catch (err) {
+        console.log('Signin callback error:', err)
+      }
     }
   }
 })
