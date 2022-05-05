ba5cadfb314e55b7bb82e477faff740a5a822f27
IceToast <social@icetoast.cloud>
2022-05-06 01:23:48 +0200

    refactor: ♻️ Extracted Form to own component

---
+++ src/pages/register/index.js
@@ -0,0 +1,110 @@
+import { useState, Fragment } from 'react'
+import Link from 'next/link'
+import {
+  Alert,
+  Box,
+  Collapse,
+  Divider,
+  Typography,
+  IconButton,
+  InputLabel,
+  CardContent,
+  Card as MuiCard
+} from '@mui/material'
+import { styled } from '@mui/material/styles'
+import themeConfig from 'src/configs/themeConfig'
+import BlankLayout from 'src/@core/layouts/BlankLayout'
+import FooterIllustrationsV1 from 'src/views/pages/auth/FooterIllustration'
+import { getProviders, signIn } from 'next-auth/react'
+
+import Google from 'mdi-material-ui/Google'
+
+// import Github from 'mdi-material-ui/Github'
+// import Twitter from 'mdi-material-ui/Twitter'
+// import Facebook from 'mdi-material-ui/Facebook'
+
+import RegistrationForm from 'src/views/register/RegisterForm'
+
+// ** Styled Components
+const Card = styled(MuiCard)(({ theme }) => ({
+  [theme.breakpoints.up('sm')]: { width: '28rem' }
+}))
+
+const LinkStyled = styled('a')(({ theme }) => ({
+  fontSize: '0.875rem',
+  textDecoration: 'none',
+  color: theme.palette.primary.main
+}))
+
+const RegisterPage = ({ providers }) => {
+  const [registerSuccess, setRegisterSuccess] = useState(false)
+  const [showSuccess, setShowSuccess] = useState(false)
+
+  return (
+    <Box className='content-center'>
+      <Card sx={{ zIndex: 1 }}>
+        <CardContent sx={{ padding: theme => `${theme.spacing(12, 9, 7)} !important` }}>
+          <Box sx={{ mb: 8, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <img alt='Logo' src='/images/Dashsite_logo.png' width={80} />
+            <Typography
+              variant='h6'
+              sx={{
+                ml: 3,
+                lineHeight: 1,
+                fontWeight: 600,
+                textTransform: 'uppercase',
+                fontSize: '1.5rem !important'
+              }}
+            >
+              {themeConfig.templateName}
+            </Typography>
+          </Box>
+          <Box sx={{ mb: 6 }}>
+            <Typography variant='h5' sx={{ fontWeight: 600, marginBottom: 1.5 }}>
+              Adventure starts here 🚀
+            </Typography>
+          </Box>
+
+          <Collapse mountOnEnter in={showSuccess} timeout='auto'>
+            <Alert severity='success'>Registration successful! Please check your email to verify your account.</Alert>
+          </Collapse>
+
+          <Collapse unmountOnExit onExited={() => setShowSuccess(true)} in={!registerSuccess} timeout='auto'>
+            <RegistrationForm successHandler={setRegisterSuccess} />
+            <Box sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'center' }}>
+              <Typography variant='body2' sx={{ marginRight: 2 }}>
+                Already have an account?
+              </Typography>
+              <Typography variant='body2'>
+                <Link passHref href='/pages/login'>
+                  <LinkStyled>Sign in instead</LinkStyled>
+                </Link>
+              </Typography>
+            </Box>
+            <Divider sx={{ my: 5 }}>or</Divider>
+            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <Link href='/' passHref>
+                <IconButton component='a' onClick={() => signIn(providers['google'].id)}>
+                  <Google sx={{ color: '#db4437' }} />
+                </IconButton>
+              </Link>
+            </Box>
+          </Collapse>
+        </CardContent>
+      </Card>
+      <FooterIllustrationsV1 />
+    </Box>
+  )
+}
+
+RegisterPage.getLayout = page => <BlankLayout>{page}</BlankLayout>
+
+export default RegisterPage
+
+export async function getServerSideProps(context) {
+  const providers = await getProviders()
+
+  return {
+    props: { providers }
+  }
+}
---
+++ src/views/register/RegisterForm.js
@@ -0,0 +1,188 @@
+import { useState } from 'react'
+import Link from 'next/link'
+
+import {
+  Alert,
+  AlertTitle,
+  Checkbox,
+  Collapse,
+  TextField,
+  List,
+  ListItem,
+  InputLabel,
+  IconButton,
+  FormControl,
+  OutlinedInput,
+  InputAdornment,
+  FormControlLabel as MuiFormControlLabel
+} from '@mui/material'
+import { styled } from '@mui/material/styles'
+import { signIn } from 'next-auth/react'
+
+import LoadingButton from '@mui/lab/LoadingButton'
+import EyeOutline from 'mdi-material-ui/EyeOutline'
+import EyeOffOutline from 'mdi-material-ui/EyeOffOutline'
+
+const LinkStyled = styled('a')(({ theme }) => ({
+  fontSize: '0.875rem',
+  textDecoration: 'none',
+  color: theme.palette.primary.main
+}))
+
+const FormControlLabel = styled(MuiFormControlLabel)(({ theme }) => ({
+  marginTop: theme.spacing(1.5),
+  marginBottom: theme.spacing(4),
+  '& .MuiFormControlLabel-label': {
+    fontSize: '0.875rem',
+    color: theme.palette.text.secondary
+  }
+}))
+
+const RegisterForm = ({ successHandler }) => {
+  const [error, setError] = useState('')
+
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
+  const [isPrivacyChecked, setIsPrivacyChecked] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleRegister = async () => {
+    setError('')
+    if (!username || !email || !password || !isPrivacyChecked) return
+    if (password !== passwordConfirm) {
+      setError('Passwords do not match')
+
+      return
+    }
+    setIsLoading(true)
+
+    const response = await fetch('/api/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, email, password })
+    })
+    setIsLoading(false)
+    if (response.status === 200) {
+      signIn('verify', { email, callbackUrl: `${window.location.origin}/pages/verify`, redirect: false })
+      successHandler(true)
+
+      return
+    }
+
+    const jsonResponse = await response.json()
+    if (response.status === 400) setError(jsonResponse.error)
+    if (response.status === 409) setError('Email already in use')
+  }
+
+  return (
+    <>
+      <Collapse in={error} timeout='auto'>
+        <Alert severity='error' sx={{ marginBottom: 4 }}>
+          <AlertTitle>Error</AlertTitle>
+          <List>
+            {error.split('. ').map((string, index) => (
+              <ListItem dense disableGutters key={index}>
+                • {string}.
+              </ListItem>
+            ))}
+          </List>
+        </Alert>
+      </Collapse>
+      <form
+        onSubmit={event => {
+          event.preventDefault()
+          handleRegister()
+        }}
+      >
+        <TextField
+          autoFocus
+          fullWidth
+          id='username'
+          label='Username'
+          value={username}
+          sx={{ marginBottom: 4 }}
+          onChange={event => setUsername(event.target.value)}
+        />
+        <TextField
+          fullWidth
+          type='email'
+          label='Email'
+          value={email}
+          sx={{ marginBottom: 4 }}
+          onChange={event => setEmail(event.target.value)}
+        />
+        <FormControl fullWidth sx={{ marginBottom: 4 }}>
+          <InputLabel htmlFor='auth-register-password'>Password</InputLabel>
+          <OutlinedInput
+            label='Password'
+            value={password}
+            id='auth-register-password'
+            onChange={event => setPassword(event.target.value)}
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position='end'>
+                <IconButton
+                  edge='end'
+                  onClick={() => setShowPassword(!showPassword)}
+                  aria-label='toggle password visibility'
+                >
+                  {showPassword ? <EyeOutline fontSize='small' /> : <EyeOffOutline fontSize='small' />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel htmlFor='auth-register-password'>Confirm Password</InputLabel>
+          <OutlinedInput
+            label='ConfirmPassword'
+            value={passwordConfirm}
+            id='auth-register-password'
+            onChange={() => setPasswordConfirm(event.target.value)}
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position='end'>
+                <IconButton
+                  edge='end'
+                  onClick={() => setShowPassword(!showPassword)}
+                  aria-label='toggle password visibility'
+                >
+                  {showPassword ? <EyeOutline fontSize='small' /> : <EyeOffOutline fontSize='small' />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <FormControlLabel
+          control={<Checkbox checked={isPrivacyChecked} onChange={() => setIsPrivacyChecked(!isPrivacyChecked)} />}
+          label={
+            <>
+              <span>I agree to </span>
+              <Link href='/' passHref>
+                <LinkStyled onClick={e => e.preventDefault()}>Privacy policy & Terms</LinkStyled>
+              </Link>
+            </>
+          }
+        />
+        <LoadingButton
+          type='submit'
+          loading={isLoading}
+          fullWidth
+          size='large'
+          variant='contained'
+          disabled={!(isPrivacyChecked && username && email && password && passwordConfirm)}
+          sx={{ marginBottom: 7 }}
+        >
+          Sign up
+        </LoadingButton>
+      </form>
+    </>
+  )
+}
+
+export default RegisterForm
