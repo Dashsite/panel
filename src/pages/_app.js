a7ad685b81256d4f4c46e8666dddada6c54e3d34
IceToast <social@icetoast.cloud>
2022-05-13 14:52:40 +0200

    chore: 💬 Updated Metadata

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,37 +11,37 @@ import '../../styles/globals.css'
 const clientSideEmotionCache = createEmotionCache()
 
 if (themeConfig.routingLoader) {
-  Router.events.on('routeChangeStart', () => {
-    NProgress.start()
-  })
-  Router.events.on('routeChangeError', () => {
-    NProgress.done()
-  })
-  Router.events.on('routeChangeComplete', () => {
-    NProgress.done()
-  })
+    Router.events.on('routeChangeStart', () => {
+        NProgress.start()
+    })
+    Router.events.on('routeChangeError', () => {
+        NProgress.done()
+    })
+    Router.events.on('routeChangeComplete', () => {
+        NProgress.done()
+    })
 }
 
 const App = ({ Component, emotionCache = clientSideEmotionCache, pageProps: { session, ...pageProps } }) => {
-  const getLayout = Component.getLayout ?? (page => <UserLayout>{page}</UserLayout>)
+    const getLayout = Component.getLayout ?? (page => <UserLayout>{page}</UserLayout>)
 
-  return (
-    <Providers emotionCache={emotionCache} session={session}>
-      {getLayout(
-        <Component {...pageProps}>
-          <Head>
-            <title>{`${themeConfig.templateName} - Material Design React Admin Template`}</title>
-            <meta
-              name='description'
-              content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
-            />
-            <meta name='keywords' content='Material Design, MUI, Admin Template, React Admin Template' />
-            <meta name='viewport' content='initial-scale=1, width=device-width' />
-          </Head>
-        </Component>
-      )}
-    </Providers>
-  )
+    return (
+        <Providers emotionCache={emotionCache} session={session}>
+            {getLayout(
+                <Component {...pageProps}>
+                    <Head>
+                        <title>{`${themeConfig.templateName}`}</title>
+                        <meta
+                            name='description'
+                            content={`${themeConfig.templateName} – The most intuitive application to manage servers.`}
+                        />
+                        <meta name='keywords' content='Pterodactyl, ProxMox, Server, proxmox, Virtualization, host' />
+                        <meta name='viewport' content='initial-scale=1, width=device-width' />
+                    </Head>
+                </Component>
+            )}
+        </Providers>
+    )
 }
 
 export default App
