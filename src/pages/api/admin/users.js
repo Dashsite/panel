c8090e029e673b18cd353550889478ca3fb8ab0d
IceToast <social@icetoast.cloud>
2022-09-14 16:15:48 +0200

    refactor: ♻️ Method Switch

--- src/pages/api/admin/users.js
+++ src/pages/api/admin/users.js
@@ -7,24 +7,20 @@ import prisma from 'src/lib/utils/PrismaClient'
  *
  */
 export default async (req, res) => {
-    switch (req.method) {
-        case 'GET':
-            // get all users from db without password
-            const users = await prisma.user.findMany({
-                select: {
-                    id: true,
-                    name: true,
-                    disabled: true,
-                    email: true,
-                    emailVerified: true,
-                    image: true,
-                    role: true,
-                    createdAt: true,
-                    updatedAt: true,
-                },
-            })
-            res.status(200).json(users)
+    if (req.method !== 'GET') return res.status(405).end()
 
-            break
-    }
+    const users = await prisma.user.findMany({
+        select: {
+            id: true,
+            name: true,
+            disabled: true,
+            email: true,
+            emailVerified: true,
+            image: true,
+            role: true,
+            createdAt: true,
+            updatedAt: true,
+        },
+    })
+    res.status(200).json(users)
 }
