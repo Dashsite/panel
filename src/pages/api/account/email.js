1d3c807f35763c0e186304c2cc5ecb3ef5764abf
IceToast <social@icetoast.cloud>
2022-09-15 00:44:17 +0200

    fix: 🐛 User Data fetching for updating email

--- src/pages/api/account/email.js
+++ src/pages/api/account/email.js
@@ -1,4 +1,6 @@
 import prisma from 'src/lib/utils/PrismaClient'
+import { email as emailValidator } from 'src/lib/validations/user'
+import { getToken } from 'next-auth/jwt'
 
 /**
  * @openapi
@@ -16,12 +18,20 @@ import prisma from 'src/lib/utils/PrismaClient'
  * @returns {Promise<void>}
  */
 export default async (req, res) => {
-    // Update Email Address
     const {
-        body: { token, email },
+        body: { email },
         method,
     } = req
-    if (method !== 'PUT') return res.status(405).end()
+    if (method !== 'PATCH') return res.status(405).end()
+
+    const token = await getToken({ req })
+
+    console.log(token.user.id)
+
+    // Validate Email
+    if (!email) return res.status(400).json({ error: 'Email is required' })
+    const ok = emailValidator.validate(email)
+    if (ok.error) return res.status(400).json({ error: ok.error.message })
 
     try {
         // Check if any user has this email address
@@ -33,14 +43,14 @@ export default async (req, res) => {
         // update user email address
         const updatedUser = await prisma.user.update({
             where: {
-                id: token.id,
+                id: token.user.id,
             },
             data: {
                 email: email,
             },
         })
 
-        res.status(200).json(updatedUser)
+        res.status(200).end()
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
