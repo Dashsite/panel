f42213d66149b625831b4598d244c9d9c424d2f3
IceToast <social@icetoast.cloud>
2022-09-14 12:45:47 +0200

    fix: 🚑️ JoiPassword Validation -> new extend API

--- src/lib/validations/user.js
+++ src/lib/validations/user.js
@@ -1,32 +1,33 @@
-import Joi from 'joi'
-import { joiPassword } from 'joi-password'
+import Joi from 'Joi'
+import { joiPasswordExtendCore } from 'joi-password'
 
-const passwordValidation = joiPassword
-  .string()
-  .min(8)
-  .max(70)
-  .minOfLowercase(1)
-  .minOfUppercase(1)
-  .minOfNumeric(1)
-  .noWhiteSpaces()
-  .required()
-  .messages({
-    'password.minOfUppercase': 'Password must contain at least one uppercase letter',
-    'password.minOfLowercase': 'Password must contain at least one lowercase letter',
-    'password.minOfNumeric': 'Password must contain at least one number',
-    'password.noWhiteSpaces': 'Password must not contain any white spaces',
-    'string.min': 'Password must be at least 8 characters long',
-    'string.max': 'Password must be at most 70 characters long'
-  })
+const JoiPassword = Joi.extend(joiPasswordExtendCore)
+
+const passwordValidation = JoiPassword.string()
+    .min(8)
+    .max(70)
+    .minOfLowercase(1)
+    .minOfUppercase(1)
+    .minOfNumeric(1)
+    .noWhiteSpaces()
+    .required()
+    .messages({
+        'password.minOfUppercase': 'Password must contain at least one uppercase letter',
+        'password.minOfLowercase': 'Password must contain at least one lowercase letter',
+        'password.minOfNumeric': 'Password must contain at least one number',
+        'password.noWhiteSpaces': 'Password must not contain any white spaces',
+        'string.min': 'Password must be at least 8 characters long',
+        'string.max': 'Password must be at most 70 characters long',
+    })
 
 const registerSchema = Joi.object().keys({
-  username: Joi.string().required().min(3).max(30),
-  email: Joi.string().email().required().messages({ 'string.email': 'Email is not valid' }),
-  password: passwordValidation
+    username: Joi.string().required().min(3).max(30),
+    email: Joi.string().email().required().messages({ 'string.email': 'Email is not valid' }),
+    password: passwordValidation,
 })
 
 const resetSchema = Joi.object().keys({
-  password: passwordValidation
+    password: passwordValidation,
 })
 
 export { registerSchema, resetSchema }
--- src/pages/api/auth/password.js
+++ src/pages/api/auth/password.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt'
-import { compile } from 'joi'
+import { compile } from 'Joi'
 import { nanoid } from 'nanoid'
 
 import sendMail from 'src/lib/utils/Nodemailer'
@@ -14,41 +14,41 @@ import { resetSchema } from 'src/lib/validations/user'
  *
  */
 const resetPassword = async req => {
-  const { token, password } = req.body
-  if (!(token && password)) throw new Error()
+    const { token, password } = req.body
+    if (!(token && password)) throw new Error()
 
-  const resetToken = await prisma.resetToken.findUnique({
-    where: {
-      token: token
+    const resetToken = await prisma.resetToken.findUnique({
+        where: {
+            token: token,
+        },
+    })
+    if (resetToken.expires < new Date()) return false
+
+    const email = resetToken.identifier
+
+    try {
+        const { error } = resetSchema.validate({ password }, { abortEarly: false, errors: { wrap: true } })
+
+        if (error) return { status: 400, message: error.message }
+
+        const passwordHash = await bcrypt.hash(password, 9)
+        await prisma.user.update({
+            where: { email },
+
+            data: {
+                password: passwordHash,
+                updatedAt: new Date(),
+            },
+        })
+
+        await prisma.resetToken.deleteMany({
+            where: {
+                identifier: email,
+            },
+        })
+    } catch (err) {
+        return false
     }
-  })
-  if (resetToken.expires < new Date()) return false
-
-  const email = resetToken.identifier
-
-  try {
-    const { error } = resetSchema.validate({ password }, { abortEarly: false, errors: { wrap: true } })
-
-    if (error) return { status: 400, message: error.message }
-
-    const passwordHash = await bcrypt.hash(password, 9)
-    await prisma.user.update({
-      where: { email },
-
-      data: {
-        password: passwordHash,
-        updatedAt: new Date()
-      }
-    })
-
-    await prisma.resetToken.deleteMany({
-      where: {
-        identifier: email
-      }
-    })
-  } catch (err) {
-    return false
-  }
 }
 
 /**
@@ -57,88 +57,88 @@ const resetPassword = async req => {
  * @returns
  */
 const resetPasswordRequest = async email => {
-  const resetToken = nanoid(48)
-  try {
-    // check if user exists
-    const user = await prisma.user.findUnique({
-      where: {
-        email
-      }
-    })
+    const resetToken = nanoid(48)
+    try {
+        // check if user exists
+        const user = await prisma.user.findUnique({
+            where: {
+                email,
+            },
+        })
 
-    if (!user) return false
+        if (!user) return false
 
-    // create resetToken and send email
-    const resetTokenDB = await prisma.resetToken.create({
-      data: {
-        identifier: email,
-        token: resetToken,
-        expires: new Date(Date.now() + 10 * 60 * 1000) // expires in 10 mnutes
-      }
-    })
+        // create resetToken and send email
+        const resetTokenDB = await prisma.resetToken.create({
+            data: {
+                identifier: email,
+                token: resetToken,
+                expires: new Date(Date.now() + 10 * 60 * 1000), // expires in 10 mnutes
+            },
+        })
 
-    const resetUrl = `${process.env.NEXTAUTH_URL}/reset?token=${resetToken}`
-    const subject = 'Reset your password'
-    const text = `
+        const resetUrl = `${process.env.NEXTAUTH_URL}/reset?token=${resetToken}`
+        const subject = 'Reset your password'
+        const text = `
     To reset your password, please click the following link:
     ${resetUrl}
   `
-    const html = `
+        const html = `
     <p>To reset your password, please click the following link:</p>
     <a href="${resetUrl}">${resetUrl}</a>
   `
 
-    await sendMail(email, subject, text, html)
+        await sendMail(email, subject, text, html)
 
-    return true
-  } catch (err) {
-    throw false
-  }
+        return true
+    } catch (err) {
+        throw false
+    }
 }
 
 export default async (req, res) => {
-  // POST Request - Sets a new password for a user and deletes all resetTokens for that user
-  if (req.method === 'POST') {
-    const result = await resetPassword(req, res)
+    // POST Request - Sets a new password for a user and deletes all resetTokens for that user
+    if (req.method === 'POST') {
+        const result = await resetPassword(req, res)
 
-    if (result.status === 400) return res.status(400).json({ error: err.message })
+        if (result.status === 400) return res.status(400).json({ error: err.message })
 
-    return result ? res.status(200).end() : res.status(500).end()
-  }
-
-  // PUT Request - Creates a new resetToken and sends an email to the user
-  if (req.method === 'PUT') {
-    const result = await resetPasswordRequest(req.body.email)
-
-    return result ? res.status(200).end() : res.status(500).end()
-  }
-
-  // GET Request - Checks if a resetToken is valid and not expired
-  if (req.method === 'GET') {
-    const resetToken = req.query.token
-    if (!resetToken) return res.status(400).end()
-
-    const resetTokenDB = await prisma.resetToken.findUnique({
-      where: {
-        token: resetToken
-      }
-    })
-
-    if (!resetTokenDB) return res.status(401).end()
-
-    // Check if token is expired - if so, delete it
-    if (resetTokenDB.expires < new Date()) {
-      await prisma.resetToken.delete({
-        where: {
-          identifier: resetTokenDB.identifier
-        }
-      })
-
-      return res.status(401).end()
+        return result ? res.status(200).end() : res.status(500).end()
     }
 
-    return res.status(200).end()
-  }
+    // PUT Request - Creates a new resetToken and sends an email to the user
+    if (req.method === 'PUT') {
+        const result = await resetPasswordRequest(req.body.email)
 
-  return res.status(405).end()
+        return result ? res.status(200).end() : res.status(500).end()
+    }
+
+    // GET Request - Checks if a resetToken is valid and not expired
+    if (req.method === 'GET') {
+        const resetToken = req.query.token
+        if (!resetToken) return res.status(400).end()
+
+        const resetTokenDB = await prisma.resetToken.findUnique({
+            where: {
+                token: resetToken,
+            },
+        })
+
+        if (!resetTokenDB) return res.status(401).end()
+
+        // Check if token is expired - if so, delete it
+        if (resetTokenDB.expires < new Date()) {
+            await prisma.resetToken.delete({
+                where: {
+                    identifier: resetTokenDB.identifier,
+                },
+            })
+
+            return res.status(401).end()
+        }
+
+        return res.status(200).end()
+    }
+
+    return res.status(405).end()
 }
