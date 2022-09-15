6052e43e1123fcf88fa005ea0eca5ef2d58d9bdb
IceToast <social@icetoast.cloud>
2022-09-15 16:47:35 +0200

    refactor: ♻️ API Routes to nextConnect

--- src/pages/api/account.js
+++ src/pages/api/account.js
@@ -1,78 +1,81 @@
 import prisma from 'src/lib/utils/PrismaClient'
+import nextConnect from 'src/lib/utils/nextConnect'
 import { getToken } from 'next-auth/jwt'
 import { userDataSchema } from 'src/lib/validations/user'
 import { validationFormatter, validationOptions } from 'src/lib/validations'
 
-/**
- * @openapi
- * /api/account:
- *  get:
- *    description: Returns the account information
- *    responses:
- *      200:
- *        description: Account information
- *    tags:
- *      - Account
- *
- * @param {import('next').NextApiRequest} req
- * @param {import('next').NextApiResponse} res
- * @returns {Promise<void>}
- */
+const handler = nextConnect()
 
-export default async (req, res) => {
-    const { method, body } = req
-    const token = await getToken({ req })
+handler.get(
+    /**
+     * @param {import('next').NextApiRequest} req
+     * @param {import('next').NextApiResponse} res
+     * @returns {Promise<void>}
+     */
+    async (req, res) => {
+        const token = await getToken({ req })
 
-    switch (method) {
-        case 'GET':
-            try {
-                // get user from token
-                const user = await prisma.user.findUnique({
-                    where: {
-                        id: token.user.id,
-                    },
-                    select: {
-                        id: true,
-                        name: true,
-                        email: true,
-                        emailVerified: true,
-                        image: true,
-                        role: true,
-                        createdAt: true,
-                        updatedAt: true,
-                    },
-                })
-                res.status(200).json(user)
-            } catch (error) {
-                res.status(500).json({ error: error.message })
-            }
-            break
-        case 'PATCH':
-            // Validate Username and email
-            const { error } = userDataSchema.validate({ ...body }, validationOptions)
-            if (error) return res.status(400).json({ error: validationFormatter(error) })
-
-            try {
-                // Check if any user has this email address
-                const user = await prisma.user.findUnique({
-                    where: { email },
-                })
-                if (user) return res.status(400).json({ error: { email: 'Email address is already in use.' } })
-
-                // update user email address
-                await prisma.user.update({
-                    where: {
-                        id: token.user.id,
-                    },
-                    data: {
-                        name: username,
-                        email: email,
-                    },
-                })
-
-                res.status(200).end()
-            } catch (error) {
-                res.status(500).json({ error: error.message })
-            }
+        try {
+            // get user from token
+            const user = await prisma.user.findUnique({
+                where: {
+                    id: token.user.id,
+                },
+                select: {
+                    id: true,
+                    name: true,
+                    email: true,
+                    emailVerified: true,
+                    image: true,
+                    role: true,
+                    createdAt: true,
+                    updatedAt: true,
+                },
+            })
+            res.status(200).json(user)
+        } catch (error) {
+            res.status(500).json({ error: error.message })
+        }
     }
-}
+)
+
+handler.patch(
+    /**
+     * @param {import('next').NextApiRequest} req
+     * @param {import('next').NextApiResponse} res
+     * @returns {Promise<void>}
+     */
+    async (req, res) => {
+        const { username, email } = req.body
+        const token = await getToken({ req })
+
+        //Validate Username and email
+        const { error } = userDataSchema.validate({ ...req.body }, validationOptions)
+        if (error) return res.status(400).json({ error: validationFormatter(error) })
+
+        try {
+            // Check if any user has this email address
+            const user = await prisma.user.findUnique({
+                where: { email },
+            })
+            if (user) return res.status(400).json({ error: { email: 'Email address is already in use.' } })
+
+            // update user email address
+            await prisma.user.update({
+                where: {
+                    id: token.user.id,
+                },
+                data: {
+                    name: username,
+                    email: email,
+                },
+            })
+
+            res.status(200).end()
+        } catch (error) {
+            res.status(500).json({ error: error.message })
+        }
+    }
+)
+
+export default handler
--- src/pages/api/admin/users.js
+++ src/pages/api/admin/users.js
@@ -1,26 +1,30 @@
 import prisma from 'src/lib/utils/PrismaClient'
+import nextConnect from 'next-connect'
 
-/**
- * @param {import('next').NextApiRequest} req
- * @param {import('next').NextApiResponse} res
- * @returns {Promise<void>}
- *
- */
-export default async (req, res) => {
-    if (req.method !== 'GET') return res.status(405).end()
+const handler = nextConnect()
 
-    const users = await prisma.user.findMany({
-        select: {
-            id: true,
-            name: true,
-            disabled: true,
-            email: true,
-            emailVerified: true,
-            image: true,
-            role: true,
-            createdAt: true,
-            updatedAt: true,
-        },
-    })
-    res.status(200).json(users)
-}
+handler.get(
+    /**
+     *
+     * @param {import('next').NextApiRequest} req
+     * @param {import('next').NextApiResponse} res
+     */
+    async (req, res) => {
+        const users = await prisma.user.findMany({
+            select: {
+                id: true,
+                name: true,
+                disabled: true,
+                email: true,
+                emailVerified: true,
+                image: true,
+                role: true,
+                createdAt: true,
+                updatedAt: true,
+            },
+        })
+        res.status(200).json(users)
+    }
+)
+
+export default handler
