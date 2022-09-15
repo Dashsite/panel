aee4f0d02a8f27885abc51715e7103a2cfef61b1
IceToast <social@icetoast.cloud>
2022-09-15 15:45:59 +0200

    feat: ✨ Added Update Account route

--- src/pages/api/account.js
+++ src/pages/api/account.js
@@ -1,5 +1,7 @@
 import prisma from 'src/lib/utils/PrismaClient'
 import { getToken } from 'next-auth/jwt'
+import { userDataSchema } from 'src/lib/validations/user'
+import { validationFormatter, validationOptions } from 'src/lib/validations'
 
 /**
  * @openapi
@@ -18,30 +20,59 @@ import { getToken } from 'next-auth/jwt'
  */
 
 export default async (req, res) => {
-    const { method } = req
-    if (method !== 'GET') return res.status(405).end()
-
+    const { method, body } = req
     const token = await getToken({ req })
 
-    try {
-        // get user from token
-        const user = await prisma.user.findUnique({
-            where: {
-                id: token.user.id,
-            },
-            select: {
-                id: true,
-                name: true,
-                email: true,
-                emailVerified: true,
-                image: true,
-                role: true,
-                createdAt: true,
-                updatedAt: true,
-            },
-        })
-        res.status(200).json(user)
-    } catch (error) {
-        res.status(500).json({ error: error.message })
+    switch (method) {
+        case 'GET':
+            try {
+                // get user from token
+                const user = await prisma.user.findUnique({
+                    where: {
+                        id: token.user.id,
+                    },
+                    select: {
+                        id: true,
+                        name: true,
+                        email: true,
+                        emailVerified: true,
+                        image: true,
+                        role: true,
+                        createdAt: true,
+                        updatedAt: true,
+                    },
+                })
+                res.status(200).json(user)
+            } catch (error) {
+                res.status(500).json({ error: error.message })
+            }
+            break
+        case 'PATCH':
+            // Validate Username and email
+            const { error } = userDataSchema.validate({ ...body }, validationOptions)
+            if (error) return res.status(400).json({ error: validationFormatter(error) })
+
+            try {
+                // Check if any user has this email address
+                const user = await prisma.user.findUnique({
+                    where: { email },
+                })
+                if (user) return res.status(400).json({ error: { email: 'Email address is already in use.' } })
+
+                // update user email address
+                await prisma.user.update({
+                    where: {
+                        id: token.user.id,
+                    },
+                    data: {
+                        name: username,
+                        email: email,
+                    },
+                })
+
+                res.status(200).end()
+            } catch (error) {
+                res.status(500).json({ error: error.message })
+            }
     }
 }
