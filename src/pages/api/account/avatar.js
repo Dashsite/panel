fb6a1a6f3e6abe018b0631797e85e57ac12bcfe2
IceToast <social@icetoast.cloud>
2022-09-16 22:29:41 +0200

    feat: ✨ Added update avatar route

--- src/pages/api/account/avatar.js
+++ src/pages/api/account/avatar.js
@@ -1,42 +1,66 @@
+import nextConnect from 'src/lib/utils/nextConnect'
+import formidable from 'formidable-serverless'
+
 import prisma from 'src/lib/utils/PrismaClient'
 import { uuid } from 'uuidv4'
-import { fs } from 'fs'
+import fs from 'fs'
+import { getToken } from 'next-auth/jwt'
 
-/**
- * @param {import('next').NextApiRequest} req
- * @param {import('next').NextApiResponse} res
- * @returns {Promise<void>}
- */
-export default async (req, res) => {
-    // Update user avatar image
-    const { body, method } = req
-    if (method !== 'PATCH') return res.status(405).end()
-
-    try {
-        console.log(body)
-
-        const imageName = uuid()
-        // get image file ending from html request
-        const imageEnding = image.split(';')[0].split('/')[1]
-        const imageFileName = `${imageName}.${imageEnding}`
-
-        const imagePath = `public/uploads/avatars/${imageFileName}`
-
-        const base64Data = image.replace(/^data:image\/png;base64,/, '')
-        await fs.writeFile(imagePath, base64Data, 'base64')
-
-        // // update user avatar image
-        // const updatedUser = await prisma.user.update({
-        //     where: {
-        //         id: token.id,
-        //     },
-        //     data: {
-        //         image,
-        //     },
-        // })
-
-        res.status(200).json({ image: imagePath })
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
+export const config = {
+    api: {
+        bodyParser: false,
+        externalResolver: true,
+    },
 }
+
+const handler = nextConnect()
+
+handler.post(
+    /**
+     * @param {import('next').NextApiRequest} req
+     * @param {import('next').NextApiResponse} res
+     * @returns {Promise<void>}
+     */
+
+    async (req, res) => {
+        const token = await getToken({ req })
+
+        const form = new formidable.IncomingForm()
+        form.uploadDir = './public/uploads/avatars'
+        form.keepExtensions = true
+        form.parse(req, async (err, fields, files) => {
+            if (err) {
+                fs.unlinkSync(path)
+
+                return res.status(500).json()
+            }
+
+            // save image path to account db avatar url
+            const { path } = files.image
+
+            console.log(path)
+
+            // concat app url to image path
+            const avatarUrl = `${process.env.APP_URL}/${path}`
+
+            try {
+                await prisma.user.update({
+                    where: {
+                        id: token.user.id,
+                    },
+                    data: {
+                        image: avatarUrl,
+                    },
+                })
+            } catch (error) {
+                // delete image from uploads folder
+                fs.unlinkSync(path)
+                res.status(500).json()
+            }
+
+            res.setHeader('Location', avatarUrl).status(201).end()
+        })
+    }
+)
+
+export default handler
