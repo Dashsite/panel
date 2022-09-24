f675b818b6a125ffd7e58d93d960d71c85213bc8
IceToast <social@icetoast.cloud>
2022-09-24 11:11:30 +0200

    feat: ✨ Added Update billing_adress route

--- src/pages/api/account/addresses/[id].js
+++ src/pages/api/account/addresses/[id].js
@@ -18,15 +18,82 @@ handler.get(
 
         try {
             //find the address by id and user id
-            const addresses = await prisma.billing_adresses.findUnique({
+            const addresses = await prisma.billing_adresses.findFirst({
                 where: {
                     id: req.query.id,
+
                     user_id: token.user.id,
                 },
             })
             if (!addresses) return res.status(404).end()
 
             return res.status(200).json(addresses)
+        } catch (error) {
+            return res.status(500).json({ error: error.message })
+        }
+    }
+)
+
+handler.put(
+    /**
+     * @param {import('next').NextApiRequest} req
+     * @param {import('next').NextApiResponse} res
+     * @returns {Promise<void>}
+     *
+     */
+    async (req, res) => {
+        const {
+            firstname,
+            lastname,
+            company_name,
+            adress_line_1,
+            adress_line_2,
+            zipcode,
+            country,
+            state,
+            phone,
+            city,
+            tax_id,
+            invoice_email,
+        } = req.body
+
+        const token = await getToken({ req })
+
+        const { error } = addressSchema.validate(req.body, validationOptions)
+        if (error) return res.status(400).json(validationFormatter(error))
+
+        // format invoice_emails to string. String is separated by semi-colon
+        const invoice_email_string = invoice_email.join(';')
+
+        try {
+            const address = await prisma.billing_adresses.update({
+                where: {
+                    AND: [
+                        {
+                            id: req.query.id,
+                        },
+                        {
+                            user_id: token.user.id,
+                        },
+                    ],
+                },
+                data: {
+                    firstname,
+                    lastname,
+                    company_name,
+                    adress_line_1,
+                    adress_line_2,
+                    zipcode,
+                    country,
+                    state,
+                    phone,
+                    city,
+                    tax_id,
+                    invoice_email: invoice_email_string,
+                },
+            })
+
+            return res.status(200).json(address)
         } catch (error) {
             return res.status(500).end()
         }
