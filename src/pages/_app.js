cb5591203eda82bd4d2a520653f8da8c84b00e6d
IceToast <social@icetoast.cloud>
2022-10-23 16:03:03 +0200

    feat: ✨ Added Frontend protected sites

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
-import { Router } from 'next/router'
+import Router from 'next/router'
 import NProgress from 'nprogress'
+import { useSession } from 'next-auth/react'
 import themeConfig from 'src/configs/themeConfig'
 import UserLayout from 'src/layouts/UserLayout'
 import { createEmotionCache } from 'src/@core/utils/create-emotion-cache'
@@ -32,20 +33,55 @@ const App = ({ Component, emotionCache = clientSideEmotionCache, pageProps: { se
     return (
         <Providers emotionCache={emotionCache} session={session}>
             {getLayout(
-                <Component {...pageProps}>
-                    <Head>
-                        <title>{`${themeConfig.templateName}`}</title>
-                        <meta
-                            name='description'
-                            content={`${themeConfig.templateName} – The most intuitive application to manage servers.`}
-                        />
-                        <meta name='keywords' content='Pterodactyl, ProxMox, Server, proxmox, Virtualization, host' />
-                        <meta name='viewport' content='initial-scale=1, width=device-width' />
-                    </Head>
-                </Component>
+                Component.adminProtected ? (
+                    <Auth>
+                        <Component {...pageProps}>
+                            <Head>
+                                <title>{`${themeConfig.templateName}`}</title>
+                                <meta
+                                    name='description'
+                                    content={`${themeConfig.templateName} – The most intuitive application to manage servers.`}
+                                />
+                                <meta
+                                    name='keywords'
+                                    content='Pterodactyl, ProxMox, Server, proxmox, Virtualization, host'
+                                />
+                                <meta name='viewport' content='initial-scale=1, width=device-width' />
+                            </Head>
+                        </Component>
+                    </Auth>
+                ) : (
+                    <Component {...pageProps}>
+                        <Head>
+                            <title>{`${themeConfig.templateName}`}</title>
+                            <meta
+                                name='description'
+                                content={`${themeConfig.templateName} – The most intuitive application to manage servers.`}
+                            />
+                            <meta
+                                name='keywords'
+                                content='Pterodactyl, ProxMox, Server, proxmox, Virtualization, host'
+                            />
+                            <meta name='viewport' content='initial-scale=1, width=device-width' />
+                        </Head>
+                    </Component>
+                )
             )}
         </Providers>
     )
 }
 
 export default App
+
+const Auth = ({ children }) => {
+    // if `{ required: true }` is supplied, `status` can only be "loading" or "authenticated"
+    const { data, status } = useSession({ required: true })
+
+    if (status === 'loading') return <div>Loading...</div>
+    if (status === 'authenticated' && data.user.role === 'admin') return children
+    if (status === 'unauthenticated') Router.push('/401')
+
+    Router.push('/403')
+
+    return <p>Redirecting...</p>
+}
