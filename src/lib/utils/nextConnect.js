f3ced808524250a48cce17c84bcc17e4172ef3e8
IceToast <social@icetoast.cloud>
2022-10-23 17:24:40 +0200

    feat: ✨ Added Logger pino

--- src/lib/utils/nextConnect.js
+++ src/lib/utils/nextConnect.js
@@ -6,8 +6,20 @@ import nextConnect from 'next-connect'
  */
 export default options => {
     let handlerOptions
-    options?.onError ?? (handlerOptions = { onError: (err, req, res) => res.status(500).json({ error: err.message }) })
-    options?.onNoMatch ?? (handlerOptions = { onNoMatch: (req, res) => res.status(405).end() })
+    options?.onError ??
+        (handlerOptions = {
+            onError: (err, req, res) => {
+                Log.error(err.message, 'Unhandled error in nextConnect handler')
+                return res.status(500).json({ error: err.message })
+            },
+        })
+    options?.onNoMatch ??
+        (handlerOptions = {
+            onNoMatch: (req, res) => {
+                Log.error(error.message, 'No matching route in nextConnect handler')
+                return res.status(405).end()
+            },
+        })
 
     return nextConnect({
         ...handlerOptions,
