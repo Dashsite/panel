1d042df41c10bbd335638be0e5f731b1d7732b30
IceToast <social@icetoast.cloud>
2022-10-23 18:27:15 +0200

    feat: 🔊 Added File logger

--- src/lib/utils/Logger.js
+++ src/lib/utils/Logger.js
@@ -2,12 +2,37 @@ import pino from 'pino'
 
 const Log = pino({
     transport: {
-        target: 'pino-pretty',
-        options: {
-            colorize: true,
-            translateTime: true,
-            ignore: 'pid,hostname',
-        },
+        targets: [
+            {
+                target: 'pino-pretty',
+                options: {
+                    colorize: true,
+                    translateTime: true,
+                    ignore: 'pid,hostname',
+                },
+            },
+            {
+                target: 'pino-pretty',
+                level: 'error',
+                options: {
+                    ignore: 'pid,hostname',
+                    colorize: false,
+                    translateTime: true,
+                    destination: './logs/error.log',
+                    mkDir: true,
+                },
+            },
+            {
+                target: 'pino-pretty',
+                options: {
+                    ignore: 'pid,hostname',
+                    colorize: false,
+                    translateTime: true,
+                    destination: './logs/info.log',
+                    mkDir: true,
+                },
+            },
+        ],
     },
     level: process.env.LOG_LEVEL || 'info',
 })
