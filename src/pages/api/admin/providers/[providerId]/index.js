06bbfc94f69785150fda38f6461759db7e3e7d85
IceToast <social@icetoast.cloud>
2022-10-28 17:12:23 +0200

    feat: ✨ Added Provider api routes

---
+++ src/pages/api/admin/providers/[providerId]/index.js
@@ -0,0 +1,108 @@
+import prisma from 'src/lib/utils/PrismaClient'
+import nextConnect from 'src/middleware'
+import Log from 'src/lib/utils/Logger'
+
+import { validationFormatter, validationOptions } from 'src/lib/validations'
+import { productProviderSchema } from 'src/lib/validations/providers'
+
+const handler = nextConnect()
+
+handler.get(
+    /**
+     * @param {import('next').NextApiRequest} req
+     * @param {import('next').NextApiResponse} res
+     * @returns {Promise<void>}
+     *
+     */
+    async (req, res) => {
+        try {
+            // get product provider by productId
+            const { providerId } = req.query
+
+            const productProvider = await prisma.product_provider.findUnique({
+                where: {
+                    id: Number(providerId),
+                },
+                include: {
+                    provider_instances: true,
+                    product_categories: true,
+                },
+            })
+
+            return res.status(200).json(productProvider)
+        } catch (error) {
+            if (error.code === 'P2025') res.status(404).json({ error: 'Product provider not found' })
+
+            Log.error(error.message, `Error getting product provider`)
+            return res.status(500).json({ error: 'Internal server error' })
+        }
+    }
+)
+
+handler.patch(
+    /**
+     * @param {import('next').NextApiRequest} req
+     * @param {import('next').NextApiResponse} res
+     * @returns {Promise<void>}
+     *
+     */
+    async (req, res) => {
+        const { name } = req.body
+        const { providerId } = req.query
+
+        // validate body
+        const { error } = productProviderSchema.validate({ ...req.body }, validationOptions)
+        if (error) return res.status(400).json({ error: validationFormatter(error) })
+
+        try {
+            // update product provider
+            const productProvider = await prisma.product_provider.update({
+                where: {
+                    id: Number(providerId),
+                },
+                data: {
+                    name,
+                },
+            })
+
+            Log.info(`Product provider ${productProvider.id} updated by ${req.session.user.id}`)
+            return res.status(200).json(productProvider)
+        } catch (error) {
+            if (error.code === 'P2025') res.status(404).json({ error: 'Product provider not found' })
+
+            Log.error(error.message, `Error updating product provider for ${req.session.user.id}`)
+            return res.status(500).json({ error: 'Internal server error' })
+        }
+    }
+)
+
+handler.delete(
+    /**
+     * @param {import('next').NextApiRequest} req
+     * @param {import('next').NextApiResponse} res
+     * @returns {Promise<void>}
+     *
+     */
+    async (req, res) => {
+        const { providerId } = req.query
+
+        try {
+            // delete product provider
+            const productProvider = await prisma.product_provider.delete({
+                where: {
+                    id: Number(providerId),
+                },
+            })
+
+            Log.info(`Product provider ${productProvider.id} deleted by ${req.session.user.id}`)
+            return res.status(200).json(productProvider)
+        } catch (error) {
+            if (error.code === 'P2025') res.status(404).json({ error: 'Product provider not found' })
+
+            Log.error(error.message, `Error deleting product provider for ${req.session.user.id}`)
+            return res.status(500).json({ error: 'Internal server error' })
+        }
+    }
+)
+
+export default handler
---
+++ src/pages/api/admin/providers/index.js
@@ -0,0 +1,66 @@
+import prisma from 'src/lib/utils/PrismaClient'
+import nextConnect from 'src/middleware'
+import Log from 'src/lib/utils/Logger'
+
+import { validationFormatter, validationOptions } from 'src/lib/validations'
+import { productProviderSchema } from 'src/lib/validations/providers'
+
+const handler = nextConnect()
+
+handler.get(
+    /**
+     * @param {import('next').NextApiRequest} req
+     * @param {import('next').NextApiResponse} res
+     * @returns {Promise<void>}
+     *
+     */
+    async (req, res) => {
+        try {
+            // get all product providers
+            const productProviders = await prisma.product_provider.findMany({
+                include: {
+                    provider_instances: true,
+                    product_categories: true,
+                },
+            })
+
+            return res.status(200).json(productProviders)
+        } catch (error) {
+            Log.error(error.message, `Error getting product providers`)
+            return res.status(500).json({ error: 'Internal server error' })
+        }
+    }
+)
+
+handler.post(
+    /**
+     * @param {import('next').NextApiRequest} req
+     * @param {import('next').NextApiResponse} res
+     * @returns {Promise<void>}
+     *
+     */
+    async (req, res) => {
+        const { name } = req.body
+
+        // validate body
+        const { error } = productProviderSchema.validate({ ...req.body }, validationOptions)
+        if (error) return res.status(400).json({ error: validationFormatter(error) })
+
+        try {
+            // create new product provider
+            const productProvider = await prisma.product_provider.create({
+                data: {
+                    name,
+                },
+            })
+
+            Log.info(`Product provider ${productProvider.id} created by ${req.session.user.id}`)
+            return res.status(200).json(productProvider)
+        } catch (error) {
+            Log.error(error.message, `Error creating product provider for ${req.session.user.id}`)
+            return res.status(500).json({ error: 'Internal server error' })
+        }
+    }
+)
+
+export default handler
