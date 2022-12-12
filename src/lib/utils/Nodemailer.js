f8262f9ed9f12d22e13b6b88d1ba8746f17d7542
IceToast <social@icetoast.cloud>
2022-12-13 00:08:05 +0100

    fix: 🐛 Destructuring mail object for easier call

--- src/lib/utils/Nodemailer.js
+++ src/lib/utils/Nodemailer.js
@@ -1,12 +1,12 @@
 import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_SERVER_HOST,
-  port: process.env.EMAIL_SERVER_PORT,
-  auth: {
-    user: process.env.EMAIL_SERVER_USER,
-    pass: process.env.EMAIL_SERVER_PASSWORD
-  }
+    host: process.env.EMAIL_SERVER_HOST,
+    port: process.env.EMAIL_SERVER_PORT,
+    auth: {
+        user: process.env.EMAIL_SERVER_USER,
+        pass: process.env.EMAIL_SERVER_PASSWORD,
+    },
 })
 
 /**
@@ -17,22 +17,21 @@ const transporter = nodemailer.createTransport({
  * @param {string} html
  * @returns {Promise} A promise that resolves when the email is sent.
  */
-const sendMail = async (email, subject, text, html) => {
-  new Promise((resolve, reject) => {
-    transporter.sendMail(
-      {
-        from: process.env.EMAIL_FROM,
-        to: email,
-        subject,
-        text,
-        html
-      },
-      (err, info) => {
-        if (err) reject(err)
-        resolve(info)
-      }
-    )
-  })
-}
+const sendMail = async ({ email, subject, text, html }) =>
+    new Promise((resolve, reject) => {
+        transporter.sendMail(
+            {
+                from: process.env.EMAIL_FROM,
+                to: email,
+                subject,
+                text,
+                html,
+            },
+            (err, info) => {
+                if (err) reject(err)
+                resolve(info)
+            }
+        )
+    })
 
 export default sendMail
