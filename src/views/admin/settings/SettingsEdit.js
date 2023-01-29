d4aa12600c636623eab3484bd11d9871ffc244e1
IceToast <social@icetoast.de>
2023-01-30 00:39:41 +0100

    feat: ✨ Use React-Final-Form

--- src/views/admin/settings/SettingsEdit.js
+++ src/views/admin/settings/SettingsEdit.js
@@ -2,54 +2,32 @@ import { useState } from 'react'
 import { Box, Switch, TextField } from '@mui/material'
 import { NumberEdit } from 'src/components/Edits/NumberEdit'
 
-const SettingsEdit = ({ option }) => {
-    const [key, optionValue] = option
-
-    const [value, setValue] = useState(optionValue)
-
-    if (typeof option === 'undefined') {
-        return null
-    }
-
-    if (typeof value === 'string') {
-        return (
-            <Box>
+const SettingsEdit = ({ field, ...props }) => {
+    const renderEdit = f => {
+        console.log(f)
+        if (f.type === 'text') {
+            return (
                 <TextField
-                    value={value}
-                    label={key}
-                    onChange={e => {
-                        setValue(e.target.value)
-                    }}
+                    fullWidth
+                    variant='outlined'
+                    // do not show any label
+                    label=''
+                    placeholder='Enter value'
+                    {...props}
                 />
-            </Box>
-        )
+            )
+        }
+
+        if (f.type === 'boolean') {
+            return <Switch {...props} />
+        }
+
+        if (f.type === 'number') {
+            return <NumberEdit fullWidth formatNumber={false} {...props} />
+        }
     }
 
-    if (typeof value === 'boolean') {
-        return (
-            <Box>
-                <Switch
-                    checked={value}
-                    label={key}
-                    onChange={e => {
-                        setValue(e.target.checked)
-                    }}
-                />
-            </Box>
-        )
-    }
-
-    if (typeof value === 'number') {
-        return (
-            <NumberEdit
-                value={Number(value)}
-                label={key}
-                onChange={e => {
-                    setValue(e.target.value)
-                }}
-            />
-        )
-    }
+    return <Box>{renderEdit(field)}</Box>
 }
 
 export default SettingsEdit
--- src/views/admin/settings/SettingsForm.js
+++ src/views/admin/settings/SettingsForm.js
@@ -1,7 +1,22 @@
 import PropTypes from 'prop-types'
-import { Box, Typography, Button } from '@mui/material'
+import { Box, Typography, Button, Tooltip, Grid } from '@mui/material'
 import { Form, Field } from 'react-final-form'
 import SettingsEdit from './SettingsEdit'
+import InformationOutline from 'mdi-material-ui/InformationOutline'
+import { styled } from '@mui/system'
+
+const GridItem = styled(Grid)(({ theme }) => ({
+    // if classname = 'center'
+    '&.center': {
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    // if classname = 'left'
+    '&.right': {
+        justifyContent: 'flex-end',
+    },
+}))
 
 const SettingsForm = ({ options }) => {
     return (
@@ -10,23 +25,42 @@ const SettingsForm = ({ options }) => {
             initialValues={options}
             render={({ handleSubmit, submitting, submitErrors }) => (
                 <form onSubmit={handleSubmit}>
-                    <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-                        {Object.entries(options).map(([key, value]) => (
-                            <Box key={key} mb={6} mr={4}>
-                                <SettingsEdit option={[key, value]} />
-                            </Box>
-                        ))}
-                    </Box>
-                    <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
-                        <Button type='submit' variant='contained' disabled={submitting}>
-                            Save
-                        </Button>
-                    </Box>
-                    {submitErrors && (
-                        <Box sx={{ marginTop: 4 }}>
-                            <Typography color='error'>{submitErrors}</Typography>
+                    <Box sx={{ marginLeft: '5rem' }}>
+                        <Grid container spacing={2} columns={12} width='100%'>
+                            {Object.entries(options).map(([key, value]) => (
+                                <Field name='key'>
+                                    {({ input, meta }) => (
+                                        <>
+                                            <GridItem item xs={4} className='center'>
+                                                <Typography>{value.label}</Typography>
+                                            </GridItem>
+                                            <GridItem item xs={1} className='center right'>
+                                                {value.description && (
+                                                    <Tooltip title={value.description}>
+                                                        <InformationOutline />
+                                                    </Tooltip>
+                                                )}
+                                            </GridItem>
+                                            <GridItem item xs={7}>
+                                                <SettingsEdit field={value} {...input} />
+                                            </GridItem>
+                                        </>
+                                    )}
+                                </Field>
+                            ))}
+                        </Grid>
+
+                        <Box sx={{ display: 'flex', justifyContent: 'flex-start' }} mt={8}>
+                            <Button type='submit' variant='contained' disabled={submitting}>
+                                Save
+                            </Button>
                         </Box>
-                    )}
+                        {submitErrors && (
+                            <Box sx={{ marginTop: 12 }}>
+                                <Typography color='error'>{submitErrors}</Typography>
+                            </Box>
+                        )}
+                    </Box>
                 </form>
             )}
         />
