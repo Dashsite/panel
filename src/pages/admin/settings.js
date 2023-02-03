0e9392b79f8bba3c5013b50ac6808d2e97e9ced7
IceToast <social@icetoast.de>
2023-02-04 00:05:59 +0100

    feat: ✨ Use Settings API

--- src/pages/admin/settings.js
+++ src/pages/admin/settings.js
@@ -1,15 +1,22 @@
 import { Typography } from '@mui/material'
 import { Box } from '@mui/system'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import Config from 'src/lib/utils/Config'
+import { useState, useEffect } from 'react'
 import SidebarList from 'src/components/Lists/SidebarList'
 import SettingsForm from 'src/views/admin/settings/SettingsForm'
-import themeConfig from 'src/configs/themeConfig'
 
-const SettingsOverview = ({ config }) => {
-    const categories = Object.keys(config)
-    const [selectedCategory, setSelectedCategory] = useState(categories[0])
+const SettingsOverview = ({}) => {
+    const [config, setConfig] = useState({})
+    const [selectedCategory, setSelectedCategory] = useState(null)
+
+    useEffect(() => {
+        const getConfig = async () => {
+            const response = await fetch('/api/admin/settings')
+            const config = await response.json()
+            setConfig(config)
+            setSelectedCategory(Object.keys(config)[0])
+        }
+        getConfig()
+    }, [])
 
     return (
         <Box>
@@ -17,40 +24,17 @@ const SettingsOverview = ({ config }) => {
             <Box sx={{ display: 'flex', flexDirection: 'row', marginTop: '1rem' }}>
                 <Box sx={{ flexShrink: 0, width: '15rem' }}>
                     <SidebarList
-                        options={categories}
+                        options={Object.keys(config)}
                         selectedOption={selectedCategory}
                         selectOption={setSelectedCategory}
                     />
                 </Box>
                 <Box p={2} ml={2} sx={{ flexGrow: 1 }}>
-                    <SettingsForm options={config[selectedCategory]} />
+                    <SettingsForm options={config[selectedCategory]} category={selectedCategory} />
                 </Box>
             </Box>
         </Box>
     )
-}
-
-export async function getServerSideProps() {
-    // TODO USE API
-
-    let config = {}
-    // get all configs from Config -> iterator over all namespaces
-    for await (const provider of Object.keys(Config)) {
-        config[provider] = {}
-        // get all configs from the namepsace -> iterator over all entries
-        for await (const [key, value] of Config[provider].iterator()) {
-            config[provider][key] = value
-        }
-    }
-
-    // sort the providers alphabetically
-    config = Object.fromEntries(Object.entries(config).sort(([a], [b]) => a.localeCompare(b)))
-
-    return {
-        props: {
-            config,
-        },
-    }
 }
 
 SettingsOverview.adminProtected = true
