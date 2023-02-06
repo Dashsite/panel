7b99aa28117136407234866a964af4bc60be85e3
IceToast <social@icetoast.de>
2023-02-07 00:25:39 +0100

    feat: ✨ Add HTML Editor to SettingsEdit

--- src/views/admin/settings/SettingsEdit.js
+++ src/views/admin/settings/SettingsEdit.js
@@ -1,9 +1,19 @@
-import { useState } from 'react'
 import { Box, Switch, TextField } from '@mui/material'
 import { NumberEdit } from 'src/components/Edits/NumberEdit'
+import dynamic from 'next/dynamic'
+
+let FullTextEditor = dynamic(() => import('src/components/Edits/FullTextEditor'), { ssr: false })
 
 const SettingsEdit = ({ field, value, ...props }) => {
     const renderEdit = () => {
+        if (field.isHtml) {
+            return (
+                <Box sx={{ width: '100%', border: '1px solid #ccc', borderRadius: 1, padding: 5 }}>
+                    {FullTextEditor && <FullTextEditor data={value} onChange={props.onChange} />}
+                </Box>
+            )
+        }
+
         if (field.type === 'string') {
             return (
                 <TextField
